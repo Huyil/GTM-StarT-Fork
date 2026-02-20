69aad888fd172477e8b08758b7480809fd79cab6
trulyno <tenodjikotaru04@gmail.com>
2026-02-20 10:46:00 +0200

    Allow rendering other modules from placeholders

--- src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
+++ src/main/java/com/gregtechceu/gtceu/data/lang/LangHandler.java
@@ -1664,6 +1664,27 @@ public class LangHandler {
                 "  {ender redstone <channel> [player_data_item_slot] -> redstone signal level",
                 "  {ender redstone <channel> <player_data_item_slot> <signal> -> sets the redstone signal outputed to the ender redstone link, returns empty string",
                 "The player_data_item_slot argument may be left empty (not 0, empty string)");
+        multiLang(provider, "gtceu.placeholder_info.module",
+                "Renders the module in the specified slot onto the central monitor (does not work in a cover)",
+                "Usage:",
+                "  {module <slot> <x> <y>} -> empty string");
+        multiLang(provider, "gtceu.placeholder_info.setImage",
+                "Sets the image URL in an image module in the specified slot",
+                "Usage:",
+                "  {setImage <slot> <url>} -> empty string");
+        multiLang(provider, "gtceu.placeholder_info.rect",
+                "Draws a rectangle at the specified position with the specified coordinates and size",
+                "Usage:",
+                "  {rect <x> <y> <width> <height> <colorARGB>} -> empty string",
+                "  {rect 0.5 0.25 2 1 0xFFFFFFFF} -> draws a white rectangle at (0.5, 0.25) with the size (2, 1)");
+        multiLang(provider, "gtceu.placeholder_info.quad",
+                "Draws a quad (must specify parameters for all 4 vertices)",
+                "Usage:",
+                "  {quad <x1> <y1> <x2> <y2> <x3> <y3> <x4> <y4> <color1> <color2> <color3> <color4>} -> empty string");
+        multiLang(provider, "gtceu.placeholder_info.item",
+                "Returns the amount and id of the item in a specified slot",
+                "Usage:",
+                "  {item <slot>} -> \"31 minecraft:diamond\" (for example)");
         multiLang(provider, "gtceu.placeholder_info.bufferText",
                 "Returns the text from a buffer accessible by ComputerCraft",
                 "Usage:",
